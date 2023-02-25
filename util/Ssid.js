791c242141c3bcf56f38ab0a4a73b47b7e9046c7
lowking <life@lowking.pro>
2023-02-25 21:03:11 +0800

    feat(ssid): no notice if the mode don't change

--- util/Ssid.js
+++ util/Ssid.js
@@ -2,20 +2,24 @@ const SSID = $network.wifi.ssid;
 const proxywifi = !$persistentStore.read("lkWifiSsids")?["ssid填这里！！！","这是第二个ssid"]:JSON.parse($persistentStore.read("lkWifiSsids"));
 let res = proxywifi.some(val => val === (!!SSID ? SSID : "cellular"));
 let lkWifiLast = !$persistentStore.read("lkWifiLast")?"abcdefghijklmnopqrstuvwxyz":$persistentStore.read("lkWifiLast");
+let preMode = !$persistentStore.read("lkWifiPreMode")?"abcdefghijklmnopqrstuvwxyz":$persistentStore.read("lkWifiPreMode");
 if (lkWifiLast!=(!!SSID ? SSID : "cellular")){
     !$persistentStore.write((!!SSID ? SSID : "cellular"), "lkWifiLast")
     if (res) {
         $surge.setOutboundMode("direct");
-        notify(1);
+        notify("1");
     } else {
         $surge.setOutboundMode("rule");
-        notify(0);
+        notify("0");
     }
 }
 
 function notify(mode) {
     setTimeout(function () {
-        !!mode ? $notification.post("Wi-Fi助手", "切换到【直接连接】", `${!!SSID ? "你的Wi-Fi：【" + SSID + "】" : "你正在使用流量"}`) : $notification.post("Wi-Fi助手", "切换到【规则模式】", `${!!SSID ? "你的Wi-Fi：【" + SSID + "】" : "你正在使用流量"}`)
+        if (preMode != mode) {
+            (mode === "1") ? $notification.post("Wi-Fi助手", "切换到【直接连接】", `${!!SSID ? "你的Wi-Fi：【" + SSID + "】" : "你正在使用流量"}`) : $notification.post("Wi-Fi助手", "切换到【规则模式】", `${!!SSID ? "你的Wi-Fi：【" + SSID + "】" : "你正在使用流量"}`)
+            !$persistentStore.write(mode, "lkWifiPreMode")
+        }
     })
 }
 
