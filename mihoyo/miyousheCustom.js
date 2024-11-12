5ea322309c29a6f3fdb751782201b371e8e79174
lowking <life@lowking.pro>
2024-11-12 19:23:16 +0800

    feat(mihoyo): supports show tower and challenge info about zzz

--- mihoyo/miyousheCustom.js
+++ mihoyo/miyousheCustom.js
@@ -1,5 +1,5 @@
 /*
-米哈游App自定义-lowking-v1.2.0
+米哈游App自定义-lowking-v1.3.0
 
 ************************
 Surge 4.2.0+ 脚本配置(其他APP自行转换配置):
@@ -7,7 +7,7 @@ Surge 4.2.0+ 脚本配置(其他APP自行转换配置):
 
 [Script]
 # > 米哈游App自定义
-米哈游我的自定义 = requires-body=1,type=http-response,pattern=https:\/\/api-takumi-record.mihoyo.com\/game_record\/card\/api\/getGameRecordCard,script-path=https://raw.githubusercontent.com/lowking/Scripts/master/mihoyo/miyousheCustom.js
+米哈游我的自定义 = requires-body=1,type=http-response,timeout=30,pattern=https:\/\/api-takumi-record.mihoyo.com\/game_record\/card\/api\/getGameRecordCard,script-path=https://raw.githubusercontent.com/lowking/Scripts/master/mihoyo/miyousheCustom.js
 米游社首页自定义 = requires-body=1,type=http-response,pattern=https:\/\/bbs-api.miyoushe.com\/apihub\/api\/home\/new,script-path=https://raw.githubusercontent.com/lowking/Scripts/master/mihoyo/miyousheCustom.js
 米游社首页tab自定义 = requires-body=1,type=http-response,pattern=https:\/\/bbs-api.miyoushe.com\/forum\/api\/getDiscussionByGame\?gids=8,script-path=https://raw.githubusercontent.com/lowking/Scripts/master/mihoyo/miyousheCustom.js
 米游社兑换中心过滤 = requires-body=1,type=http-response,pattern=https:\/\/bbs-api.miyoushe.com\/common\/homushop\/v1\/web\/goods\/list,script-path=https://raw.githubusercontent.com/lowking/Scripts/master/mihoyo/miyousheCustom.js
@@ -22,13 +22,26 @@ const homeTopBarKey = 'homeTopBarKey'
 const homePageTabKey = 'homePageTabKey'
 const goodsRegFilterKey = 'goodsRegFilterKey'
 const coffeeTopPostFilterKey = 'coffeeTopPostFilterKey'
+const salt6xKey = 'salt6xKey'
+const saltK2Key = 'saltK2Key'
+const zzzBbsCookieKey = 'zzzBbsCookieKey'
+const zzzCookieKey = 'zzzCookieKey'
+const appVersionKey = 'appVersionKey'
+const zzzDfpKey = 'zzzDfpKey'
 const regionGames = lk.getVal(regionGamesKey, "")
 const homeTopBar = lk.getVal(homeTopBarKey, "")
 const homePageTab = lk.getVal(homePageTabKey, "")
 const goodsRegFilter = lk.getVal(goodsRegFilterKey, "")
 const coffeeTopPostFilter = lk.getVal(coffeeTopPostFilterKey, "")
+let salt6x = lk.getVal(salt6xKey, "t0qEgfub6cvueAPgR5m9aQWWVciEer7v")
+let saltK2 = lk.getVal(saltK2Key, "rtvTthKxEyreVXQCnhluFgLXPOFKPHlA")
+const zzzBbsCookie = lk.getVal(zzzBbsCookieKey)
+const zzzCookie = lk.getVal(zzzCookieKey)
+const appVersion = lk.getVal(appVersionKey, "2.71.1")
+const zzzDfp = lk.getVal(zzzDfpKey)
+const MD5 = function(d){result = M(V(Y(X(d),8*d.length)));return result.toLowerCase()};function M(d){for(var _,m="0123456789ABCDEF",f="",r=0;r<d.length;r++)_=d.charCodeAt(r),f+=m.charAt(_>>>4&15)+m.charAt(15&_);return f}function X(d){for(var _=Array(d.length>>2),m=0;m<_.length;m++)_[m]=0;for(m=0;m<8*d.length;m+=8)_[m>>5]|=(255&d.charCodeAt(m/8))<<m%32;return _}function V(d){for(var _="",m=0;m<32*d.length;m+=8)_+=String.fromCharCode(d[m>>5]>>>m%32&255);return _}function Y(d,_){d[_>>5]|=128<<_%32,d[14+(_+64>>>9<<4)]=_;for(var m=1732584193,f=-271733879,r=-1732584194,i=271733878,n=0;n<d.length;n+=16){var h=m,t=f,g=r,e=i;f=md5_ii(f=md5_ii(f=md5_ii(f=md5_ii(f=md5_hh(f=md5_hh(f=md5_hh(f=md5_hh(f=md5_gg(f=md5_gg(f=md5_gg(f=md5_gg(f=md5_ff(f=md5_ff(f=md5_ff(f=md5_ff(f,r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+0],7,-680876936),f,r,d[n+1],12,-389564586),m,f,d[n+2],17,606105819),i,m,d[n+3],22,-1044525330),r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+4],7,-176418897),f,r,d[n+5],12,1200080426),m,f,d[n+6],17,-1473231341),i,m,d[n+7],22,-45705983),r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+8],7,1770035416),f,r,d[n+9],12,-1958414417),m,f,d[n+10],17,-42063),i,m,d[n+11],22,-1990404162),r=md5_ff(r,i=md5_ff(i,m=md5_ff(m,f,r,i,d[n+12],7,1804603682),f,r,d[n+13],12,-40341101),m,f,d[n+14],17,-1502002290),i,m,d[n+15],22,1236535329),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+1],5,-165796510),f,r,d[n+6],9,-1069501632),m,f,d[n+11],14,643717713),i,m,d[n+0],20,-373897302),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+5],5,-701558691),f,r,d[n+10],9,38016083),m,f,d[n+15],14,-660478335),i,m,d[n+4],20,-405537848),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+9],5,568446438),f,r,d[n+14],9,-1019803690),m,f,d[n+3],14,-187363961),i,m,d[n+8],20,1163531501),r=md5_gg(r,i=md5_gg(i,m=md5_gg(m,f,r,i,d[n+13],5,-1444681467),f,r,d[n+2],9,-51403784),m,f,d[n+7],14,1735328473),i,m,d[n+12],20,-1926607734),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+5],4,-378558),f,r,d[n+8],11,-2022574463),m,f,d[n+11],16,1839030562),i,m,d[n+14],23,-35309556),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+1],4,-1530992060),f,r,d[n+4],11,1272893353),m,f,d[n+7],16,-155497632),i,m,d[n+10],23,-1094730640),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+13],4,681279174),f,r,d[n+0],11,-358537222),m,f,d[n+3],16,-722521979),i,m,d[n+6],23,76029189),r=md5_hh(r,i=md5_hh(i,m=md5_hh(m,f,r,i,d[n+9],4,-640364487),f,r,d[n+12],11,-421815835),m,f,d[n+15],16,530742520),i,m,d[n+2],23,-995338651),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+0],6,-198630844),f,r,d[n+7],10,1126891415),m,f,d[n+14],15,-1416354905),i,m,d[n+5],21,-57434055),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+12],6,1700485571),f,r,d[n+3],10,-1894986606),m,f,d[n+10],15,-1051523),i,m,d[n+1],21,-2054922799),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+8],6,1873313359),f,r,d[n+15],10,-30611744),m,f,d[n+6],15,-1560198380),i,m,d[n+13],21,1309151649),r=md5_ii(r,i=md5_ii(i,m=md5_ii(m,f,r,i,d[n+4],6,-145523070),f,r,d[n+11],10,-1120210379),m,f,d[n+2],15,718787259),i,m,d[n+9],21,-343485551),m=safe_add(m,h),f=safe_add(f,t),r=safe_add(r,g),i=safe_add(i,e)}return Array(m,f,r,i)}function md5_cmn(d,_,m,f,r,i){return safe_add(bit_rol(safe_add(safe_add(_,d),safe_add(f,i)),r),m)}function md5_ff(d,_,m,f,r,i,n){return md5_cmn(_&m|~_&f,d,_,r,i,n)}function md5_gg(d,_,m,f,r,i,n){return md5_cmn(_&f|m&~f,d,_,r,i,n)}function md5_hh(d,_,m,f,r,i,n){return md5_cmn(_^m^f,d,_,r,i,n)}function md5_ii(d,_,m,f,r,i,n){return md5_cmn(m^(_|~f),d,_,r,i,n)}function safe_add(d,_){var m=(65535&d)+(65535&_);return(d>>16)+(_>>16)+(m>>16)<<16|65535&m}function bit_rol(d,_){return d<<_|d>>>32-_}
 
-const main = () => {
+const main = async () => {
     if (!lk.isRequest()) {
         lk.boxJsJsonBuilder({
             "icons": [
@@ -252,18 +265,47 @@ const main = () => {
         }, {
             regionNames: ","
         })
-        let ret = resp.data.list.reduce((acc, cur) => {
-            const regionName = `,${cur["region_name"]},`
-            lk.log(`我的-卡片项目：${cur["region_name"]}`)
+        let ret = []
+        for (let card of resp.data.list) {
+            const regionName = `,${card["region_name"]},`
+            lk.log(`我的-卡片项目：${card["region_name"]}`)
             if (regionGamesMap.regionNames.includes(regionName)) {
-                let regionUrl = regionGamesMap[cur["region_name"]]
+                let regionUrl = regionGamesMap[card["region_name"]]
                 if (regionUrl && regionUrl.startsWith("http")) {
-                    cur.url = regionUrl
+                    card.url = regionUrl
                 }
-                acc.push(cur)
+                if (regionName == ",新艾利都,") {
+                    const roleId = card["game_role_id"]
+                    // 获取爬塔数据
+                    const indexInfo = await getIndexInfo(roleId)
+                    let layer = "-"
+                    if (indexInfo?.data?.stats?.climbing_tower_layer) {
+                        layer = `${indexInfo?.data?.stats?.climbing_tower_layer}`
+                    }
+                    card.data[2].name = "鏖战纪录"
+                    card.data[2].value = layer
+                    // 获取式與防卫战数据
+                    let r1, r2 = []
+                    const challengeInfo = await getChallengeInfo(roleId, 1)
+                    if (challengeInfo?.data?.rating_list) {
+                        r1 = challengeInfo.data.rating_list.reduce((acc, cur) => {
+                            acc.push(`${cur.times}${cur.rating}`)
+                            return acc
+                        }, [])
+                    }
+                    card.data[3].name = "式與防卫战"
+                    const preChallengeInfo = await getChallengeInfo(roleId, 2)
+                    if (preChallengeInfo?.data?.rating_list) {
+                        r2 = preChallengeInfo.data.rating_list.reduce((acc, cur) => {
+                            acc.push(`${cur.times}${cur.rating}`)
+                            return acc
+                        }, [])
+                    }
+                    card.data[3].value = `${r1.length == 0 ? "-" : r1.join(" / ")} | ${r2.length == 0 ? "-" : r2.join(" / ")}`
+                }
+                ret.push(card)
             }
-            return acc
-        }, [])
+        }
         if (ret.length == 0) {
             return false
         }
@@ -272,6 +314,57 @@ const main = () => {
         lk.done({body: JSON.stringify(resp)})
     }
     return true
+}
+
+const getHeader = () => {
+    let reqHeaders = Object.keys($request.headers).reduce((obj, key) => {
+        obj[key.toLowerCase()] = $request.headers[key]
+        return obj
+    }, {})
+    let respHeaders = Object.keys($response.headers).reduce((obj, key) => {
+        obj[key.toLowerCase()] = $response.headers[key]
+        return obj
+    }, {})
+    reqHeaders.cookie = `${reqHeaders.cookie}${respHeaders["set-cookie"]}`
+    return reqHeaders
+}
+
+const getIndexInfo = async (roleId) => {
+    lk.log(`正在获取${roleId}的战绩信息`)
+    let queryString = `role_id=${roleId}&server=prod_gf_cn`
+    let headers = {
+        referer: "https://app.mihoyo.com",
+        "x-rpc-device_fp": zzzDfp,
+        "x-rpc-client_type": 2,
+        "x-rpc-app_version": appVersion,
+        ds: getDs("", "", queryString),
+        cookie: `${zzzCookie}${zzzBbsCookie}`
+    }
+    return await lk.req.get({
+        url: `https://api-takumi-record.mihoyo.com/event/game_record_zzz/api/zzz/index?${queryString}`,
+        headers
+    }).then(({error, resp, data}) => {
+        return JSON.parse(data)
+    })
+}
+
+const getChallengeInfo = async (roleId, scheduleType) => {
+    lk.log(`正在获取${roleId}的挑战信息`)
+    let queryString = `role_id=${roleId}&schedule_type=${scheduleType}&server=prod_gf_cn`
+    let headers = {
+        referer: "https://app.mihoyo.com",
+        "x-rpc-device_fp": zzzDfp,
+        "x-rpc-client_type": 2,
+        "x-rpc-app_version": appVersion,
+        ds: getDs("", "", queryString),
+        cookie: `${zzzCookie}${zzzBbsCookie}`
+    }
+    return await lk.req.get({
+        url: `https://api-takumi-record.mihoyo.com/event/game_record_zzz/api/zzz/challenge?${queryString}`,
+        headers
+    }).then(({error, resp, data}) => {
+        return JSON.parse(data)
+    })
 }
 
 const sortByArray = (source, refer, key) => {
@@ -288,10 +381,44 @@ const sortByArray = (source, refer, key) => {
     }, [])
 }
 
-if(!lk.isExecComm) {
-    if (!main()) {
-        lk.done()
+const getDs = (task, body="", query="") => {
+    let randomStr = lk.randomString(6)
+    let timestamp = Math.floor(Date.now() / 1000)
+    let sign, ds, str
+    switch (task) {
+        case "signIn":
+            const randomInt = Math.floor(Math.random() * (200000 - 100001) + 100001)
+            str = `salt=${salt6x}&t=${timestamp}&r=${randomInt}&b=${body}&q=${query}`
+            sign = MD5(str)
+            ds = `${timestamp},${randomInt},${sign}`
+            break
+        case "dailyCheckin":
+            str = `salt=${salt6x}&t=${timestamp}&r=${randomStr}`
+            if (body || query) {
+                str = `${str}&b=${body}&q=${query}`
+            }
+            sign = MD5(str)
+            ds = `${timestamp},${randomStr},${sign}`
+            break
+        default:
+            str = `salt=${saltK2}&t=${timestamp}&r=${randomStr}`
+            if (body || query) {
+                str = `${str}&b=${body}&q=${query}`
+            }
+            sign = MD5(str)
+            ds = `${timestamp},${randomStr},${sign}`
     }
+    lk.log(`getDs: task: ${task}, body: ${body}, query: ${query}, str: ${str}`)
+
+    return ds
+}
+
+if(!lk.isExecComm) {
+    main().then((ret) => {
+        if (!ret) {
+            lk.done()
+        }
+    })
 }
 
 //ToolKit-start
