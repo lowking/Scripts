167298a813a4764acf2ebfd79c4ca9c8fb804e8c
lowking <life@lowking.pro>
2024-11-19 15:31:08 +0800

    refactor(tk): convert to arrow function

--- util/ToolKit.js
+++ util/ToolKit.js
@@ -69,10 +69,10 @@ function ToolKit(scriptName, scriptId, options) {
 
     return new (class {
         constructor(scriptName, scriptId, options) {
-            Object.prototype.s = function (replacer, space) {
+            Object.prototype.s = (replacer, space) => {
                 return JSON.stringify(this, replacer, space)
             }
-            String.prototype.o = function (reviver) {
+            String.prototype.o = (reviver) => {
                 return JSON.parse(this, reviver)
             }
             this.userAgent = `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0.2 Safari/605.1.15`
