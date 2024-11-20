895a2a0e5ccf18f4077f57c5ffb802be0806efc2
lowking <life@lowking.pro>
2024-11-20 14:40:03 +0800

    fix(tk): parse object error on nodejs

--- util/ToolKit.js
+++ util/ToolKit.js
@@ -75,7 +75,6 @@ function ToolKit(scriptName, scriptId, options) {
                 return JSON.stringify(this, replacer, space)
             }
             Object.prototype.o = function (reviver) {
-                if (typeof this === "object") return this
                 return JSON.parse(this, reviver)
             }
             this.userAgent = `Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0.2 Safari/605.1.15`
